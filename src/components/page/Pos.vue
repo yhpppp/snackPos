4f5e9074a35b3e634a8ef04cb4391e6f14a5f057
yhpp <yhpp1995@gmail.com>
2018-05-04 11:45:49 +0800

    add Pos.vue

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{mes}}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        mes: 'hello,pos'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
